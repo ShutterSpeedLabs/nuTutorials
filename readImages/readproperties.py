e0b660b9dfeb71c4d250f10b408cc622a2b92c41
ShutterSpeed Labs <shutterspeedlabs@gmail.com>
2024-06-29 09:47:07 +0530

    added pynb

--- readImages/readproperties.py
+++ readImages/readproperties.py
@@ -1,9 +1,9 @@
 from nuimages import NuImages
 
-nuim = NuImages(dataroot='/media/parashuram/AutoData/nuImagesMini/', version='v1.0-mini', verbose=True, lazy=True)
+nuim = NuImages(dataroot='/media/parashuram/AutoData/nuImagesMini/', version='v1.0-train', verbose=True, lazy=True)
 
 nuim.category[0]
 
 print(nuim.category[0])
 
-print(nuim.table_names)
+print("table name:", nuim.table_names)
